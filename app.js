5527ef1ca71600ee9a4510b93608d84ad122e6e1
dharmavirkumar <dharmavirkumar1215@gmail.com>
2025-08-04 01:10:30 +0530

    route fix error

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ app.set("view engine","ejs")
 app.use(express.static("public"))
 app.use(express.urlencoded({extended:true}))
 
-app.use("/user",route)
+app.use("/user",route);
 app.get("/",(req,res)=>{
-    res.render("Dasebord")
+    res.redirect("/user/");
 })
 const port = process.env.PORT || 5000;
 app.listen(port,(req,res)=>{
